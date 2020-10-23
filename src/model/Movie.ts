08000db791255d9cfc2bbb0e33975e25ef447a96
Lourenço Passos <lo.passos93@gmail.com>
2020-10-22 23:41:15 -0300

    project setup

---
+++ src/model/Movie.ts
@@ -0,0 +1,55 @@
+export class Movie{
+    constructor(
+    private id: string,
+    private title: string,
+    private director: string,
+    private available: boolean
+    ){}
+
+    getId(){
+        return this.id;
+    }
+
+    getTitle(){
+        return this.title
+    }
+
+    getDirector(){
+        return this.director;
+    }
+
+    getAvailable() {
+        return this.available
+    }
+
+
+    setId(id: string){
+        this.id = id;
+    }
+
+    setTitle(title: string){
+        this.title = title;
+    }
+
+    setDirector(director: string){
+        this.director = director;
+    }
+
+    setAvailable(available: boolean) {
+        this.available = available;
+    }
+
+
+
+    static toUserModel(movie: any): Movie {
+        return new Movie(movie.id, movie.title, movie.director, movie.available);
+      }
+
+
+}
+
+export interface MovieInputDTO{
+    title: string;
+    director: string;
+}
+
